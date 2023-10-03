0fea0f752d2765a72c54cebe840ec219002dc64b
Jhonnatan <jhonnatan.jncp@gmail.com>
2023-10-03 19:33:24 -0300

    feat: function handleSubmit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,14 @@
 import { login } from "./modules/utils.js";
 import { useState } from "./modules/utils.js";
-const emailInput = document.getElementById("email")
-const passwordInput = document.getElementById("password")
+
 const loginButton = document.getElementsByClassName("btn")
-
-const [email, setEmail] = useState("")
-const [password, setpassword] = useState("")
-
-const controlEmail = (event) => {
-    console.log(event)
-}
-
-const controlPassword = (event) => {
-    console.log(event)
-}
+const [error, setError] = useState(null);
+const [requesting, setRequesting] = useState(false);
 
 const handleSubmit = () => {
-    console.log("submited")
+    const values = {email: email, password: password};
+    login(values).then().catch(()=>{
+        
+    }).finally(()=>{alert("chegou")})
 }
+loginButton.addEventListener('click', handleSubmit())
